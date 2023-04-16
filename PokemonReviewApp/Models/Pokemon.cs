6776a71f7452c8104a291bef269ee9857c95dd41
Sebastian265 <sebastian265@wp.pl>
2023-04-17 00:19:34 +0200

    Edit

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -7,6 +7,6 @@
         public DateTime BirthDate { get; set; }
         public ICollection<Review> Reviews { get; set; }
         public ICollection<PokemonOwner> PokemonOwners { get; set; }
-        public ICollection<PokemonCategory> PokemonCategories { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; } 
     }
 }
